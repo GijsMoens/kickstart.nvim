e385c66f509563cfcc7e234b9b7cc99638dbc9c3
GijsMoens <gijs360@gmail.com>
2024-11-20 14:47:07 +0100

    last changes

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -1,29 +1,12 @@
---
--- Shows how to use the DAP plugin to debug your code.
---
--- Primarily focused on configuring the debugger for Go, but can
--- be extended to other languages as well. That's why it's called
--- kickstart.nvim and not kitchen-sink.nvim ;)
-
 return {
-  -- NOTE: Yes, you can install new plugins here!
   'mfussenegger/nvim-dap',
-  -- NOTE: And you can specify dependencies as well
   dependencies = {
-    -- Creates a beautiful debugger UI
     'rcarriga/nvim-dap-ui',
-
-    -- Required dependency for nvim-dap-ui
     'nvim-neotest/nvim-nio',
-
     'ldelossa/nvim-dap-projects',
     'theHamsta/nvim-dap-virtual-text',
-
-    -- Installs the debug adapters for you
     'williamboman/mason.nvim',
     'jay-babu/mason-nvim-dap.nvim',
-
-    -- Add your own debuggers here
     'leoluz/nvim-dap-go',
     'mfussenegger/nvim-dap-python',
   },
@@ -31,7 +14,6 @@ return {
     local dap = require 'dap'
     local dapui = require 'dapui'
     return {
-      -- Function keys for main DAP actions
       { '<F3>', dapui.toggle, desc = 'DAP toggle UI' },
       { '<F4>', dap.pause, desc = 'DAP pause (thread)' },
       { '<F5>', dap.continue, desc = 'DAP launch or continue' },
@@ -47,8 +29,6 @@ return {
         desc = 'DAP run last',
       },
       { '<F12>', dap.terminate, desc = 'DAP terminate' },
-
-      -- Leader-based mappings for extended DAP actions
       {
         '<leader>dd',
         function()
@@ -85,95 +65,115 @@ return {
           dap.repl.toggle()
         end,
         desc = 'DAP toggle debugger REPL',
-      }, -- Optional if not using dap-ui
-
+      },
       {
         '<leader>dk',
         function()
           dap.down()
         end,
-        { noremap = true, silent = true, desc = 'DAP down' },
+        desc = 'DAP down',
       },
       {
         '<leader>dj',
         function()
           dap.up()
         end,
-        { noremap = true, silent = true, desc = 'DAP up' },
+        desc = 'DAP up',
       },
-      -- Additional mappings (optional)
+      {
+        '<leader>dl',
+        function()
+          dapui.close()
+          dapui.setup()
+          dapui.open()
+        end,
+        desc = 'DAP reset UI layout',
+      },
       unpack(keys),
     }
   end,
   config = function()
     local dap = require 'dap'
     local dapui = require 'dapui'
+    local widgets = require 'dap.ui.widgets'
+
+    -- Python configuration example
+    dap.configurations.python = {
+      {
+        type = 'python',
+        request = 'launch',
+        name = 'inference',
+        program = '/home/g.moens/nnUNet/nnunetv2/run/run_training.py',
+        args = {
+          '203',
+          '2d',
+          '0',
+          '-device',
+          'cuda',
+          '-tr',
+          'DS4Trainer',
+          '--npz',
+          '--val',
+          '--val_with',
+          '/data/groups/beets-tan/g.moens/PI-CAI_results/Dataset203_picai_zonal_nocrop/DS4Trainer__nnUNetPlans__2d/fold_0/checkpoint_final.pth',
+        },
+        console = 'integratedTerminal',
+        justMyCode = false,
+      },
+      {
+        type = 'python',
+        request = 'launch',
+        name = 'validate_plain',
+        program = '/home/g.moens/nnUNet/nnunetv2/run/run_training.py',
+        args = { '203', '2d', '0', '-device', 'cuda', '-tr', 'DS4Trainer', '--npz' },
+        console = 'integratedTerminal',
+        justMyCode = false,
+      },
+    }
 
     require('mason-nvim-dap').setup {
       automatic_installation = true,
-      handlers = {},
       ensure_installed = { 'delve' },
     }
 
     dap.set_log_level 'DEBUG'
-    dap.defaults.php.exception_breakpoints = { 'Notice', 'Warning', 'Error', 'Exception' }
     dapui.setup {
-      icons = {
-        expanded = '▾',
-        collapsed = '▸',
-        current_frame = '⚪', -- Changed for better visibility
-      },
-      controls = {
-        enabled = true, -- Show controls by default for easy stepping
-        icons = {
-          pause = '⏸',
-          play = '▶',
-          step_into = '⏎',
-          step_over = '⏭',
-          step_out = '⏮',
-          step_back = '🔙', -- Updated for clarity
-          run_last = '🔄', -- Clear icon for rerun
-          terminate = '⏹',
-          disconnect = '⏏',
-        },
-      },
       layouts = {
         {
-          elements = {
-            { id = 'scopes', size = 0.4 }, -- Larger size for easy viewing of model parameters, gradients
-            { id = 'watches', size = 0.2 },
-            { id = 'breakpoints', size = 0.2 },
-            { id = 'stacks', size = 0.2 },
-          },
-          size = 50, -- Adjusted for a good balance
+          elements = { 'scopes', 'watches', 'breakpoints', 'stacks' },
+          size = 50,
           position = 'left',
         },
         {
-          elements = {
-            'repl', -- Moved REPL to top for quick access to variable states
-            'console',
-          },
-          size = 15, -- Larger console for debugging printouts
+          elements = { 'repl', 'console' },
+          size = 15,
           position = 'bottom',
         },
       },
-      floating = {
-        border = 'rounded', -- Rounded borders for better aesthetics
-      },
-      render = {
-        max_type_length = nil, -- Remove any length limit to view full data structures
-      },
+      floating = { border = 'rounded' },
     }
 
-    local status, err = pcall(function()
-      dofile(vim.fn.expand '~/nnUNet/nnunetv2/tests/nvim-dap.lua')
-    end)
+    require('nvim-dap-projects').config_paths = { '/home/g.moens/nnUNet/nnunetv2/tests/nvim-dap.lua' }
 
-    if not status then
-      print('Error loading nvim-dap.lua: ', err)
+    -- Custom REPL formatting
+    local function custom_repr(value)
+      if type(value) == 'table' and value.__repr__ then
+        return value:__repr__()
+      else
+        return vim.inspect(value)
+      end
     end
 
-    require('nvim-dap-projects').config_paths = { '/home/g.moens/nnUNet/nnunetv2/tests/nvim-dap.lua' }
+    dap.repl.commands = {
+      custom_print = function(expression)
+        local value = assert(loadstring('return ' .. expression))()
+        print(custom_repr(value))
+      end,
+      expand = function(expression)
+        local value = assert(loadstring('return ' .. expression))()
+        print(vim.inspect(value))
+      end,
+    }
 
     dap.listeners.after.event_initialized['dapui_config'] = function()
       dapui.open()
@@ -184,23 +184,8 @@ return {
     dap.listeners.before.event_exited['dapui_config'] = function()
       dapui.close()
     end
-    -- Lock 'q' and fix pane sizes
-    vim.api.nvim_create_autocmd('FileType', {
-      pattern = { 'dapui_scopes', 'dapui_breakpoints', 'dapui_stacks', 'dapui_watches', 'dapui_repl', 'dapui_console' },
-      callback = function()
-        vim.api.nvim_buf_set_keymap(0, 'n', 'q', '', { noremap = true, silent = true }) -- Disable `q` key
-        vim.cmd 'setlocal winfixwidth'
-        vim.cmd 'setlocal winfixheight'
-      end,
-    })
 
-    -- Golang specific config
-    require('dap-go').setup {
-      delve = {
-        detached = vim.fn.has 'win32' == 0,
-      },
-    }
-
+    require('dap-go').setup()
     require('dap-python').setup '/home/g.moens/miniconda3/envs/cuda12_env/bin/python'
   end,
 }
